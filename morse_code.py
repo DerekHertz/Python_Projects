f6aed357fd8f081ae813e6c7ccd46c1241ee25a2
derek <97211702+DerekHertz@users.noreply.github.com>
2024-07-15 16:13:53 -0700

    Create morse_code.py
    morse code translator

---
+++ morse_code.py
@@ -0,0 +1,56 @@
+
+morse_code = {
+    "A": ".-",
+    "B": "-...",
+    "C": "-.-.",
+    "D": "-..",
+    "E": ".",
+    "F": "..-.",
+    "G": "--.",
+    "H": "....",
+    "I": "..",
+    "J": ".---",
+    "K": "-.-",
+    "L": ".-..",
+    "M": "--",
+    "N": "-.",
+    "O": "---",
+    "P": ".--.",
+    "Q": "--.-",
+    "R": ".-.",
+    "S": "...",
+    "T": "-",
+    "U": "..-",
+    "V": "...-",
+    "W": ".--",
+    "X": "-..-",
+    "Y": "-.--",
+    "Z": "--..", 
+    "1": ".----",
+    "2": "..---",
+    "3": "...--",
+    "4": "....-",
+    "5": ".....",
+    "6": "-....",
+    "7": "--...",
+    "8": "---..",
+    "9": "----.",
+    "0": "-----",
+}
+
+
+def translate_input(user_input):
+
+    new_string = ''
+
+    for letter in user_input:
+        if letter == " ":
+            new_string += " "
+        else:
+            new_string += morse_code.get(letter, "")+ " "
+
+    print(new_string.strip())
+
+
+user_input = input("What word or sentence would you like to translate?" ).upper()
+translate_input(user_input)
